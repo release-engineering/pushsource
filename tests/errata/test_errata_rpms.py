6613862db63779ebc3c0117125681c734d2ab2a1
Rohan McGovern <rmcgover@redhat.com>
2020-02-19 09:05:41 +1000

    Extend errata source rpm tests
    Do some validation of the produced RPM push items.

--- tests/errata/test_errata_rpms.py
+++ tests/errata/test_errata_rpms.py
@@ -227,3 +227,121 @@ def test_errata_rpms_via_koji(fake_errata_tool, fake_koji, koji_dir):
             module=None,
         )
     ]
+
+    # Erratum should be sent to every dest referenced by an RPM
+    rpm_dests = []
+    for item in rpm_items:
+        rpm_dests.extend(item.dest)
+    assert set(errata_item.dest) == set(rpm_dests)
+
+    # It should have found the RPMs referenced by the advisory
+    assert sorted(rpm_items, key=lambda rpm: rpm.src) == [
+        RpmPushItem(
+            name="sudo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/ppc64le/sudo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            ),
+            dest=["rhel-8-for-ppc64le-baseos-e4s-rpms__8_DOT_0"],
+            md5sum="0d56f302617696d3511e71e1669e62c0",
+            sha256sum="31c4f73af90c6d267cc5281c59e4a93ae3557b2253d9a8e3fef55f3cafca6e54",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-debuginfo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/ppc64le/sudo-debuginfo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            ),
+            dest=["rhel-8-for-ppc64le-baseos-e4s-debug-rpms__8_DOT_0"],
+            md5sum="e242826fb38f487502cdc1f1a06991d2",
+            sha256sum="04db0c39efb31518ff79bf98d1c27256d46cdc72b967a5b2094a6efec3166df2",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-debuginfo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/ppc64le/sudo-debuginfo-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            ),
+            dest=["rhel-8-for-ppc64le-baseos-e4s-debug-rpms__8_DOT_0"],
+            md5sum="e242826fb38f487502cdc1f1a06991d2",
+            sha256sum="04db0c39efb31518ff79bf98d1c27256d46cdc72b967a5b2094a6efec3166df2",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-debugsource-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/ppc64le/sudo-debugsource-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            ),
+            dest=["rhel-8-for-ppc64le-baseos-e4s-debug-rpms__8_DOT_0"],
+            md5sum="d6da7e2e3d9efe050fef2e8d047682be",
+            sha256sum="355cbb9dc348b17782cff57120391685d6a1f6884facc54fac4b7fb54abeffba",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-debugsource-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/ppc64le/sudo-debugsource-1.8.25p1-4.el8_0.3.ppc64le.rpm",
+            ),
+            dest=["rhel-8-for-ppc64le-baseos-e4s-debug-rpms__8_DOT_0"],
+            md5sum="d6da7e2e3d9efe050fef2e8d047682be",
+            sha256sum="355cbb9dc348b17782cff57120391685d6a1f6884facc54fac4b7fb54abeffba",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-1.8.25p1-4.el8_0.3.src.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/src/sudo-1.8.25p1-4.el8_0.3.src.rpm",
+            ),
+            dest=[
+                "rhel-8-for-ppc64le-baseos-e4s-source-rpms__8_DOT_0",
+                "rhel-8-for-x86_64-baseos-e4s-source-rpms__8_DOT_0",
+            ],
+            md5sum="f94ab3724b498e3faeab643fe2a67c9c",
+            sha256sum="10d7724302a60d0d2ca890fc7834b8143df55ba1ce0176469ea634ac4ab7aa28",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+        RpmPushItem(
+            name="sudo-1.8.25p1-4.el8_0.3.x86_64.rpm",
+            state="PENDING",
+            src=os.path.join(
+                koji_dir,
+                "packages/sudo/1.8.25p1/4.el8_0.3/data/signed",
+                "fd431d51/x86_64/sudo-1.8.25p1-4.el8_0.3.x86_64.rpm",
+            ),
+            dest=["rhel-8-for-x86_64-baseos-e4s-rpms__8_DOT_0"],
+            md5sum="25e9470c4fe96034fe1d7525c04b5d8e",
+            sha256sum="593f872c1869f7beb963c8df2945fc691a1d999945c8c45c6bc7e02731fa016f",
+            origin="RHSA-2020:0509",
+            build="sudo-1.8.25p1-4.el8_0.3",
+            signing_key="fd431d51",
+        ),
+    ]

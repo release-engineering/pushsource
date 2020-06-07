8600518cf4c01e2a8bea858017a20388b0eb6725
Rohan McGovern <rmcgover@redhat.com>
2020-06-08 09:43:18 +1000

    Add validators and converters on base & erratum push items

--- src/pushsource/_impl/model/conv.py
+++ src/pushsource/_impl/model/conv.py
@@ -2,13 +2,17 @@
 import datetime
 import logging
 from functools import partial
+import re
 
 import six
 from frozenlist2 import frozenlist
 from dateutil import tz
 
+from .. import compat_attr as attr
+
 
 LOG = logging.getLogger("pushsource")
+HEX_PATTERN = re.compile(r"^[0-9a-f]+$")
 
 
 def sloppylist(value, elem_converter=None):
@@ -62,3 +66,47 @@ def datestr(value):
             raise ValueError("can't parse %s as a date" % repr(value))
 
     return value
+
+
+def hexstr(length, value):
+    if value is None:
+        return value
+
+    msg = "can't parse %s as a hex string" % repr(value)
+
+    if not isinstance(value, six.string_types):
+        raise TypeError(msg)
+
+    if len(value) != length:
+        raise ValueError("wrong length of %s (expected: %s)" % (repr(value), length))
+
+    value = value.lower()
+    if not HEX_PATTERN.match(value):
+        raise ValueError(msg)
+
+    return value
+
+
+md5str = partial(hexstr, 32)
+sha1str = partial(hexstr, 40)
+sha256str = partial(hexstr, 64)
+
+
+def upper_if_str(value):
+    if isinstance(value, six.string_types):
+        return value.upper()
+    return value
+
+
+def int2str(value):
+    # If value is an integer, convert it to string instead.
+    # Mostly used for "numeric string" fields on errata, such as pushcount.
+    if isinstance(value, int):
+        return str(value)
+    return value
+
+
+instance_of = attr.validators.instance_of
+instance_of_str = instance_of(six.string_types)
+optional = attr.validators.optional
+optional_str = optional(instance_of_str)

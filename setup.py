fcc52e2cc75ba0638e0bf96d6b5cda725195ad49
Rohan McGovern <rmcgover@redhat.com>
2020-06-08 11:46:48 +1000

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushsource",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,

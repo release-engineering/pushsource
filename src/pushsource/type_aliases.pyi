4fcd04771e1a13ab9efded638d1867497d221594
Ryan Smith <avlwx2014@gmail.com>
2022-01-11 10:17:22 -0500

    Add missing Json* types

--- src/pushsource/type_aliases.pyi
+++ src/pushsource/type_aliases.pyi
@@ -1,7 +1,17 @@
 import datetime
-from typing import Union, Collection
+from collections import Mapping, Sequence
+from numbers import Number
+from typing import Union, Collection, Optional
 
 Date = datetime.date
 DateTime = datetime.datetime
 
 MaybeString = Union[str, Collection[str]]
+
+# First attempt at a JsonObject type based on the spec from
+# https://www.json.org/json-en.html
+# from the spec a Value can be a string, number, object, array, true, false, or null
+# the "null" case is covered by Optional[]
+_JsonValue = Optional[Union[str, Number, object, "_JsonArray", bool]]
+_JsonArray = Sequence[_JsonValue]
+JsonObject = Mapping[str, _JsonValue]

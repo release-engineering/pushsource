ce175ce29fc4475b01392de9f79a709d5b449ce1
Rohan McGovern <rmcgover@redhat.com>
2022-03-16 17:10:49 +1000

    Promote examples/list-push-items to a real command
    This example script dumps whatever's found when given a particular
    pushsource URL. I'd like to promote it to a proper command,
    pushsource-ls, which is installed by pip. It seems to be quite useful as
    a development tool and also in demonstrating this library's concepts.
    
    Compared to the example script, some minor changes have been made:
    
    - a simple config file is introduced; hardcoded URLs were OK for an
      example, but it should be properly configurable for a real command
    
    - don't default to black format since not everyone will have that
      installed
    
    - tweak it to be python2-compatible

---
+++ tests/cmd/test_list_cmd.py
@@ -0,0 +1,162 @@
+import os
+import sys
+import logging
+import textwrap
+
+import yaml
+import pytest
+
+from pushsource import Source
+
+from pushsource._impl.list_cmd import (
+    format_python_black,
+    main as list_main,
+    default_format,
+    format_python_black,
+)
+from pushsource._impl import list_cmd
+
+STAGED_DATA_DIR = os.path.join(os.path.dirname(__file__), "../staged/data")
+
+
+@pytest.fixture(autouse=True)
+def reset():
+    # Reset sources after every test, since we'll be registering backends
+    # as we load config.
+    Source.reset()
+
+    # Reset pushsource logger which may have been set to DEBUG
+    # if we ran any command with --debug.
+    logging.getLogger("pushsource").setLevel(logging.NOTSET)
+
+
+@pytest.fixture()
+def black_noop(monkeypatch, tmpdir):
+    """A fixture which, when used, forces black command to succeed
+    (though doesn't actually format anything).
+    """
+
+    monkeypatch.setenv("PATH", str(tmpdir))
+
+    black = tmpdir.join("black")
+    black.write('#!/bin/sh\necho "$@"\n')
+    black.chmod(0o755)
+
+
+@pytest.fixture()
+def black_error(monkeypatch, tmpdir):
+    """A fixture which, when used, forces black command to fail."""
+
+    monkeypatch.setenv("PATH", str(tmpdir))
+
+    black = tmpdir.join("black")
+    black.write("#!/bin/sh\nexit 30\n")
+    black.chmod(0o755)
+
+
+def test_typical(monkeypatch, capsys):
+    """A typical usage of pushsource-ls works as expected."""
+
+    monkeypatch.setattr(sys, "argv", ["", "staged:%s/simple_files" % STAGED_DATA_DIR])
+
+    # It should run OK
+    list_main()
+
+    # The output should contain push item stuff.
+    # Just sample it.
+    (out, _) = capsys.readouterr()
+    assert "FilePushItem(" in out
+    assert "My wonderful ISO" in out
+
+
+def test_valid_yaml(monkeypatch, capsys):
+    """'--format yaml' generates valid YAML."""
+
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        ["", "--debug", "--format", "yaml", "staged:%s/simple_files" % STAGED_DATA_DIR],
+    )
+
+    # It should run OK
+    list_main()
+
+    # The output should be valid YAML.
+    (out, _) = capsys.readouterr()
+    data = yaml.safe_load(out)
+
+    # It should be a list.
+    assert isinstance(data, list)
+
+    # Not gonna check it all, but they should be dicts reproducing the
+    # PushItem model.
+    assert isinstance(data[0]["FilePushItem"]["name"], str)
+
+
+def test_uses_config(monkeypatch, capsys, tmpdir):
+    """pushsource-ls loads custom sources from a config file."""
+
+    monkeypatch.setenv("HOME", str(tmpdir))
+    tmpdir.mkdir(".config").join("pushsource.conf").write(
+        textwrap.dedent(
+            """
+                sources:
+                - name: list-cmd-test
+                  url: staged:threads=2
+            """
+        ).strip()
+    )
+    monkeypatch.setattr(
+        sys, "argv", ["", "list-cmd-test:%s/simple_files" % STAGED_DATA_DIR]
+    )
+
+    # It should run OK
+    list_main()
+
+    # It should load as normal, thus proving that list-cmd-test was
+    # registered from config.
+    (out, _) = capsys.readouterr()
+    assert "FilePushItem(" in out
+
+
+def test_bad_config(monkeypatch, caplog, tmpdir):
+    """pushsource-ls gives a reasonable error if config is invalid."""
+
+    monkeypatch.setenv("HOME", str(tmpdir))
+    tmpdir.mkdir(".config").join("pushsource.conf").write(
+        "oops, [ this ain't valid YAML is it?"
+    )
+    monkeypatch.setattr(sys, "argv", ["", "any:thing"])
+
+    # It should exit
+    with pytest.raises(SystemExit) as excinfo:
+        list_main()
+
+    # With this exit code
+    assert excinfo.value.code == 52
+
+    # And it should have told us why
+    assert "Error loading config" in caplog.text
+
+
+def test_default_format_black(black_noop):
+    """pushsource-ls picks python-black as default format if black is working."""
+
+    assert default_format() == "python-black"
+
+
+def test_default_format_noblack(black_error):
+    """pushsource-ls picks python as default format if black is not working."""
+
+    assert default_format() == "python"
+
+
+def test_format_black_error(black_error):
+    """black formatter raises if black command fails"""
+
+    # It should raise
+    with pytest.raises(RuntimeError) as excinfo:
+        format_python_black("some-object")
+
+    # It should tell us something about what went wrong
+    assert "Cannot format with black, exit code 30" in str(excinfo.value)

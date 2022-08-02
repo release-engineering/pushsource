eebb30ebb431271f46be0daabf38987ed4d0b8bd
Rohan McGovern <rmcgover@redhat.com>
2022-08-03 08:30:54 +1000

    Release 2.22.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushsource",
-    version="2.22.3",
+    version="2.22.4",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,

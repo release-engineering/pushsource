b32afc26b87d55d87a411f4cddd8d50c905bac23
Jindrich Luza <jluza@redhat.com>
2022-08-31 08:48:47 +0200

    Tolerated requests.exceptions.ConnectionError when status_code is 404
    For registry_source tolerate also ConnectionError which is raised when
    Session runs out of retries

--- src/pushsource/_impl/backend/registry_source.py
+++ src/pushsource/_impl/backend/registry_source.py
@@ -108,7 +108,7 @@ class RegistrySource(Source):
                         manifest_types=[mtype],
                     )
                     self._manifests[source_uri][mtype] = manifest_details
-                except requests.exceptions.HTTPError as e:
+                except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as e:
                     if e.response.status_code == 404:
                         continue
                     raise e

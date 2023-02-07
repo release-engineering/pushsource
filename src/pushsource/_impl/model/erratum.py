b3d9a8023f90c7361522ed4f84c5231ab698e074
Jindrich Luza <jluza@redhat.com>
2023-02-07 17:26:02 +0100

    Added container_list attribute to Erratum model

--- src/pushsource/_impl/model/erratum.py
+++ src/pushsource/_impl/model/erratum.py
@@ -265,6 +265,12 @@ class ErratumPushItem(PushItem):
     :type: list[ErratumPackageCollection]
     """
 
+    container_list = attr.ib(type=list, default=attr.Factory(frozenlist), converter=frozenlist)
+    """A list of containers ssociated with the advisory.
+
+    :type: list[str]
+    """
+
     from_ = attr.ib(type=str, default=None, validator=optional_str)
     """Contact email address for the owner of the advisory.
 
@@ -368,11 +374,19 @@ class ErratumPushItem(PushItem):
         if data.get("cdn_repo"):
             kwargs["dest"] = data["cdn_repo"]
 
+        # If source of data is ET/staging directory, try to get container_list directly
+        if data.get("container_list"):
+            kwargs["container_list"] = data["container_list"]
+
         # If there are content type hints, copy those while dropping the
         # pulp-specific terminology
         pulp_user_metadata = data.get("pulp_user_metadata") or {}
         if pulp_user_metadata.get("content_types"):
             kwargs["content_types"] = pulp_user_metadata["content_types"]
+
+        # If source of data is pulp, try to get container_list from pulp_user_metadata
+        if pulp_user_metadata.get("container_list"):
+            kwargs["container_list"] = pulp_user_metadata["container_list"]
 
         kwargs["references"] = ErratumReference._from_data(data.get("references") or [])
 

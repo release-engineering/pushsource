9a6871baf7239f8c066678198edd508e79d82aa9
Jindrich Luza <jluza@redhat.com>
2023-03-01 08:25:25 +0100

    Fixed frozendict convertor to core.frozendict (#311)
    * Fixed frozendict convertor to core.frozendict
    
    validation of container_list in erratum model is set to check if
    instance of frozen dict is frozendict.core.frozendict which is not "compatible"
    with frozendict.frozendict
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/pushsource/_impl/model/conv.py
+++ src/pushsource/_impl/model/conv.py
@@ -5,7 +5,7 @@ from functools import partial
 import re
 
 from frozenlist2 import frozenlist
-from frozendict import frozendict
+from frozendict.core import frozendict  # pylint: disable=no-name-in-module
 from dateutil import tz
 
 from .. import compat_attr as attr

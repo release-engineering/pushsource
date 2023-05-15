d1af4ad638defa5f340f2283c0081ab11ab42cf4
Emily Zheng <yuzheng@redhat.com>
2023-05-15 13:08:12 +0800

    Release 2.32.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushsource",
-    version="2.31.0",
+    version="2.32.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,

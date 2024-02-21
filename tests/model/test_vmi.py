07e5186ad2b53cd7754ec25c9b7cfa90d25c0f15
Jonathan Gangi <jgangi@redhat.com>
2024-02-21 16:43:23 -0300

    Add attribute "marketplace_title" to VMIRelease
    This commit introduces a new optional parameter on `VMIPushItem` model
    called `marketplace_title_template` which is going to be used in
    conjunction with `VMIPushItem.marketplace_title` property
    by the pubtool as the marketplace "version title" whenever it's set, by
    overriding the usual version title which the pubtool was going to
    compute.
    
    This is required for some layered products which requires a
    specialized templating title instead of the one generated by the
    pubtool.
    
    With this, it will be possible to transport the template via the
    VMIPushItem from the source to the pubtooling which will format it into
    the apropriate value for the marketaplace.
    
    Refers to SPSTRAT-116

--- tests/model/test_vmi.py
+++ tests/model/test_vmi.py
@@ -1,6 +1,6 @@
 from pytest import raises
 
-from pushsource import VMIRelease
+from pushsource import VMIRelease, VMIPushItem
 
 
 def test_invalidate_datestr():
@@ -9,3 +9,36 @@ def test_invalidate_datestr():
         VMIRelease(product="myprod", arch="x86_64", respin=1, date="not-a-real-date")
 
     assert "can't parse 'not-a-real-date' as a date" in str(exc_info.value)
+
+
+def test_marketplace_title():
+    """Ensure the marketplace_title is properly formed from the template."""
+
+    release = VMIRelease(
+        product="myprod", arch="x86_64", version="7.0", respin=1, date="20240101"
+    )
+
+    # Template provided
+    template = r"Test {product} {arch} {version} - success"
+    pi = VMIPushItem(
+        description="mydescription",
+        name="myname",
+        marketplace_title_template=template,
+        release=release,
+    )
+    assert pi.marketplace_title == "Test myprod x86_64 7.0 - success"
+
+    # No template provided
+    pi = VMIPushItem(
+        description="mydescription",
+        name="myname",
+        release=release,
+    )
+    assert pi.marketplace_title == ""
+
+    # No release provided
+    pi = VMIPushItem(
+        description="mydescription",
+        name="myname",
+    )
+    assert pi.marketplace_title == ""

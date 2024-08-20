44f7b6edb46c98098c6876f411b3fce621ecb599
Rohan McGovern <rmcgover@redhat.com>
2024-08-21 09:38:15 +1000

    Release 2.47.1 (add missing version bump)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushsource",
-    version="2.47.0",
+    version="2.47.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,

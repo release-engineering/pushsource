c93b6ab67723c9278b1a8de62bc75f8726a581f9
Robert Bikar <rbikar@redhat.com>
2025-12-11 08:17:30 +0100

    Release v2.52.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements():
 
 setup(
     name="pushsource",
-    version="2.51.2",
+    version="2.52.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
